c349ea2b50a06f4bb447bf41fc44d8fd4ba71a55
randell-gamedev <navaretterandell.gamedev@gmail.com>
2026-01-25 21:35:37 +0800

    carousel-animation-checkpoint

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var carouselTimer = 0;
 var pageIndex = 0;
 
+
 //HTML References
 const characterName = document.getElementById("character-name");
 const genderText = document.getElementById("gender");
@@ -10,7 +11,9 @@ const visionText = document.getElementById("vision");
 const weaponText = document.getElementById("weapon");
 const regionText = document.getElementById("region");
 const loreText = document.getElementById("lore");
-const cardImg = document.getElementById("character-card");
+const inactiveLeft = document.getElementById("inactive-card-left");
+const activeImg = document.getElementById("active-card");
+const inactiveRight = document.getElementById("inactive-card-right");
 const visionImg = document.getElementById("character-vision");
 const bodyBG = document.querySelector("body")
 const nextButton = document.getElementById("next-button");
@@ -100,7 +103,6 @@ const genshinDirectory = [
 //Carousel Intervals
 setInterval(() => {
   carouselTimer++;
-  console.log(carouselTimer);
 
   if(carouselTimer == 5) {
     CarouselNext();
@@ -116,13 +118,31 @@ function InitializeButtons() {
 }
 
 function PageUpdate() {
+    var inactiveIndexLeft = pageIndex - 1;
+    var inactiveIndexRight = pageIndex + 1;
+
     characterName.textContent = genshinDirectory[pageIndex].name;
     genderText.textContent = genshinDirectory[pageIndex].gender;
     visionText.textContent = genshinDirectory[pageIndex].vision;
     weaponText.textContent = genshinDirectory[pageIndex].weapon;
     regionText.textContent = genshinDirectory[pageIndex].region;
     //loreText.textContent = genshinDirectory[pageIndex].lore;
-    cardImg.src = genshinDirectory[pageIndex].portrait;
+    
+    if (inactiveIndexLeft < 0) {
+        inactiveIndexLeft = genshinDirectory.length - 1;
+        inactiveLeft.src = genshinDirectory[inactiveIndexLeft].portrait;
+    } else {
+        inactiveLeft.src = genshinDirectory[inactiveIndexLeft].portrait;
+    }
+
+    if (inactiveIndexRight > genshinDirectory.length - 1) {
+        inactiveIndexRight = 0;
+        inactiveRight.src = genshinDirectory[inactiveIndexRight].portrait;
+    } else {
+        inactiveRight.src = genshinDirectory[inactiveIndexRight].portrait;
+    }
+
+    activeImg.src = genshinDirectory[pageIndex].portrait;
     visionImg.src = genshinDirectory[pageIndex].visionIcon;
     bodyBG.style.backgroundImage = genshinDirectory[pageIndex].wallpaper;
     carouselTimer = 0;
